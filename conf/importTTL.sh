b7471490e34bf14cbfcf6b6b46a759136d06355d
timo.homburg <timo.homburg@hs-mainz.de>
2021-06-02 20:18:29 +0200

    Added Docker file parameters

---
+++ conf/importTTL.sh
@@ -0,0 +1,13 @@
+#connect "http://localhost:8080/rdf4j-server".
+show repositories.
+create native-shacl.
+atlantgis
+atlantgisstore
+10000
+spoc
+
+open atlantgis.
+load "/opt/proj4rdf.ttl".
+load "/opt/proj4rdf_data.ttl".
+close.
+quit.
