9cbbd0167d8059c482a7073306f5242920c6a258
Timo <situx@users.noreply.github.com>
2022-08-25 18:28:28 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -425,7 +425,7 @@ function rewriteLink(thelink){
         curlocpath=window.location.href.replace(baseurl,"")
         rest=thelink.replace(baseurl,"")
     }
-    if(!(rest.endswith("/"))){
+    if(!(rest.endsWith("/"))){
         rest+="/"
     }
     console.log(rest)
